460f96cad654266b078ddc63eb9b28d86e29a077
Vanya Tochevska <Vanya@Mac.communityfibre.co.uk>
2024-12-05 13:36:07 +0000

    added activated route to edit existing reservation, updated styling

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReservationService } from '../reservation/reservation.service';
 import { Reservation } from '../models/reservation';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-reservation-form',
@@ -15,7 +15,8 @@ export class ReservationFormComponent implements OnInit{
   constructor(
       private formBuilder: FormBuilder,
       private reservationService: ReservationService,
-      private router: Router) {}
+      private router: Router,
+      private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.reservationForm = this.formBuilder.group({
@@ -24,13 +25,29 @@ export class ReservationFormComponent implements OnInit{
       guestName: ['', Validators.required],
       guestEmail: ['', [Validators.required, Validators.email]],
       roomNumber: ['', Validators.required],
-    })
+    });
+
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    if(id){
+      let reservation = this.reservationService.getReservation(id);
+      if(reservation){
+        this.reservationForm.patchValue(reservation)
+      }
+    }
   }
 
   onSubmit() {
     if (this.reservationForm.valid){
       let reservation: Reservation = this.reservationForm.value;
-      this.reservationService.addReservation(reservation);
+
+      let id = this.activatedRoute.snapshot.paramMap.get('id');
+      if(id){
+        this.reservationService.updateReservation(id, reservation)
+        
+      } else {
+        this.reservationService.addReservation(reservation)
+      }
+
       this.router.navigate(['/list'])
     }
   }
