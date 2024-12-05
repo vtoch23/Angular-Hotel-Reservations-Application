73614a9a519205edcb00caf9506843c616529fdb
Vanya Tochevska <Vanya@Mac.communityfibre.co.uk>
2024-12-05 11:54:54 +0000

    added functionality to reservation service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
+import { ReservationListComponent } from '../reservation-list/reservation-list.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
+  private reservations: Reservation [] = [];
 
-  constructor() { }
+  getReservations(): Reservation[] {
+    return this.reservations;
+  }
+
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find(res => res.id === id);
+  }
+
+  addReservation(reservation: Reservation): void {
+    this.reservations.push(reservation);
+  }
+
+  deleteReservation(id: string): void {
+    let index = this.reservations.findIndex(res => res.id === id);
+    this.reservations.splice(index, 1);
+  }
+
+  updateReservation(updatedReservation: Reservation): void {
+    let index = this.reservations.findIndex(res => res.id === updatedReservation.id);
+    this.reservations[index] = updatedReservation;
+  } 
 }
