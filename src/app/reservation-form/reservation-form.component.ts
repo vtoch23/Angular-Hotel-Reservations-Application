700d34ccdfba2136ef99b279f19de5ddb70b229b
Vanya Tochevska <Vanya@Mac.communityfibre.co.uk>
2024-12-05 12:04:30 +0000

    updated reservation form validation and injected service for submitting a reservation

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
 
 @Component({
   selector: 'app-reservation-form',
@@ -9,21 +11,26 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ReservationFormComponent implements OnInit{
   reservationForm: FormGroup = new FormGroup({});
 
-  constructor(private formBuilder: FormBuilder){}
+  constructor(
+      private formBuilder: FormBuilder,
+      private reservationService: ReservationService) {
+
+    }
 
   ngOnInit(): void {
     this.reservationForm = this.formBuilder.group({
       checkInDate: ['', Validators.required],
       checkOutDate: ['', Validators.required],
       guestName: ['', Validators.required],
-      guestEmail: ['', Validators.required, Validators.email],
+      guestEmail: ['', [Validators.required, Validators.email]],
       roomNumber: ['', Validators.required],
     })
   }
 
   onSubmit() {
     if (this.reservationForm.valid){
-      console.log('valid')
+      let reservation: Reservation = this.reservationForm.value;
+      this.reservationService.addReservation(reservation)
     }
   }
 
