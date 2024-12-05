b3f65e7603a73974c09d61cca65ab9dd1d81dbb9
Vanya Tochevska <Vanya@Mac.communityfibre.co.uk>
2024-12-05 11:17:06 +0000

    added app configuration and components for reservation list, reservation form and home component with home module and reservation module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    HomeModule,
+    ReservationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
